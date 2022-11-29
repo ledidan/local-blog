5045cfb732e9f85e942619a4a34a42f1f8c5557a
ledidan <didan.mobe@gmail.com>
2022-11-29 21:03:06 +0700

    Add code

--- src/store.ts
+++ src/store.ts
@@ -9,5 +9,5 @@ export const store = configureStore({
 
 // Get RootState and AppDispatch from our store
 export type RootState = ReturnType<typeof store.getState>
-
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
